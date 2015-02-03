584ffdc4313f0181de64c3642deabbb21112ff57
heyjane <janemsheppard@janes-mbp.hsd1.il.comcast.net>
2015-02-03 13:48:19 -0600

    Uploading modified/new files with my specific info and formatting.

--- js/helper.js
+++ js/helper.js
@@ -13,21 +13,24 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr/>';
+var HTMLheaderRole = '<span id="role">%data%</span><hr/>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
-var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
-var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="gray-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="gray-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="gray-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="gray-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="gray-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="gray-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="gray-text">%data%</span></li>';
+var HTMLfooter = '<li class="flex-item"><span class="footImg"><img src="%imageSource%" class="img-responsive"></span><span class="footer-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLWelcomeMsg = '<span class="welcome-message">%data%</span>';
 
+var HTMLmenuOptions = '<li><a href="#"><img src="%data%" class="img-responsive"></a></li>';
+
 var HTMLskillsStart = '<h3 id="skillsH3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><span class="gray-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -36,11 +39,11 @@ var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
+var HTMLprojectStart = '<div class="project-entry col-md-4"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<a href="#"><img src="%data%" class="img-responsive" alt="Project Image"></a>';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
@@ -52,7 +55,7 @@ var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
@@ -65,7 +68,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName(name) || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -85,7 +88,10 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
 });
 
 
@@ -170,7 +176,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map,marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -233,11 +239,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,150 @@
+var bio = {
+  "name" : "Jane Sheppard",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "773.405.0989",
+    "email" : "janemsheppard@gmail.com",
+    "github" : "heyjane",
+    "twitter" : "twitter-id",
+    "location" : "Chicago"
+  },
+  "welcomeMessage" : '<br>"Good design is making something intelligible and memorable.<br>  Great design is making something memorable and meaningful." <br>- Dieter Rams',
+  "skills" : [
+  "JavaScript", "HTML5", "CSS", "Python", "UX", "Integration"
+  ],
+  "biopic" : "images/janeBioPic.png"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "University of Illinois",
+      "location": "Urbana-Champaign",
+      "degree": "Bachelor of Science in Engineering",
+      "majors": "Engineering with a Concentration in Marketing and Management",
+      "dates": "1982",
+      "url" : "http://engineering.illinois.edu/",
+    }
+  ],
+
+  "onlineCourses" : [
+   {
+      "title" : "Front-End Developer Nanodegree",
+      "school" : "Udacity",
+      "date" : "2015",
+      "url" : "http://www.udacity.com/course/nd001"
+    },
+     {
+      "title" : "Object-Oriented JavaScript",
+      "school" : "Udacity",
+      "date" : "2015",
+      "url" : "http://www.udacity.com/course/ud015"
+    },
+    {
+      "title" : "JavaScript Basics",
+      "school" : "Udacity",
+      "date" : "2015",
+      "url" : "http://www.udacity.com/course/ud804"
+    },
+    {
+      "title" : "Intro to jQuery",
+      "school" : "Udacity",
+      "date" : "2015",
+      "url" : "http://www.udacity.com/course/ud245"
+    },
+    {
+      "title" : "Intro to HTML and CSS",
+      "school" : "Udacity",
+      "date" : "2014",
+      "url" : "http://www.udacity.com/course/ud304"
+    },
+    {
+      "title" : "How to Use Git and GitHub",
+      "school" : "Udacity",
+      "date" : "2014",
+      "url" : "http://www.udacity.com/course/ud775"
+    },
+    {
+      "title" : "Introduction to Computer Science",
+      "school" : "Udacity",
+      "date" : "2012",
+      "url" : "http://www.udacity.com/course/cs101"
+    }
+  ]
+};
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "JS Consulting",
+      "title" : "Independent Consultant",
+      "location" : "Chicago",
+      "dates" : "2011-present",
+      "description" : "Evolved early stage businesses from ideation to execution.  Strategized and implemented transformational solutions in marketing automation, performance management, goal alignment, project management, CRM, sales pipeline management, systems integration, mobile and web-based productivity tools, workflow management, and process efficiency.  Reduced expenses and increased functional productivity or in small business clients across multiple industries."
+    },
+    {
+      "employer" : "Sheppard Homeschool",
+      "title" : "Curriculum Developer/Home Educator",
+      "location" : "Chicago",
+      "dates" : "1993-2013",
+      "description" : "Researched, developed, and implemented curricula for pre-K through grade 12. Provided consulting services to charter schools to define scope and content for accelerated learning programs.  Managed home school education outreach programs in partnership with Lyric Opera of Chicago, Chicago Symphony Orchestra, and the Art Institute of Chicago.  Successful students include a University of Chicago full academic scholarship graduate."
+    },
+    {
+      "employer" : "Digital Equipment Corporation",
+      "title" : "Sales Support Account Manager",
+      "location" : "Chicago",
+      "dates" : "1986-1993",
+      "description" : "Managed technical sales support for the Quaker Oats account in Chicago.  Developed strategic plans, prepared proposals, delivered technical training to internal sales teams and external clients, and managed customer technology support.  Created and implemented software development project schedules and technical documentation plans.  Drafted, edited, and published marketing and technical documentation for software and hardware product releases."
+    },
+    {
+      "employer" : "Airfone, Inc.",
+      "title" : "Software Systems Engineer",
+      "location" : "Oak Brook",
+      "dates" : "1984-1986",
+      "description" : "Evaluated software design proposals for system compatibility and feasibility.  Delivered in-flight testing, flight crew training, and system maintenance for a public in-flight telephone service.  Wrote and implemented a detailed pilot system evaluation plan.  Developed and delivered a national field technician training seminar.  Created detailed design specifications for a public telephone system for rail travel between New York and Washington, D.C"
+    },
+    {
+      "employer" : "University of Illinois at Urbana-Champaign",
+      "title" : "Instructor",
+      "location" : "Urbana-Champaign",
+      "dates" : "1984",
+      "description" : "Taught Engineering Graphics and Design.  Supervised a design and drafting laboratory.  Cited in UIUC list of excellent professors and instructors."
+    },
+    {
+      "employer" : "Arthur Andersen & Co.",
+      "title" : "Systems Consultant",
+      "location" : "Chicago",
+      "dates" : "1983-1984",
+      "description" : "Delivered system analyst and consulting services to corporate clients.  Developed a Project Control System for client billing and time management.  Organized a nationwide training seminar for Information Processing Systems."
+    }
+  ]
+}
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "Mockup to Website",
+      "dates" : "December 2014",
+      "description" : "Replicated a design mockup (PDF-file) in HTML and CSS as part of Udacity's Front-End Web Developer Nanodegree coursework.",
+      "images" :"images/mug.png",
+      "url" : "https://github.com/heyjane/Udacity-FrontEnd-Project-One.git"
+    },
+    {
+      "title" : "Southeast Asia Tour Blog",
+      "dates" : "December 2012 - January 2013",
+      "description" : "Created a travel blog to share backpacking travels in Southeast Asia as part of a homeschool abroad trip.",
+      "images" : "images/tripography.png",
+      "url" : "http://www.tripography.weebly.com/"
+    },
+    {
+      "title" : "Botajuste",
+      "dates" : "March 2014",
+      "description" : "Designed a website to explore the market potential for petite-sized boots.  Website includes a forms survey and blog.",
+      "images" : "images/boot.jpg",
+      "url" : "http://www.botajuste.com/"
+    }
+  ]
+};
+
+
+
